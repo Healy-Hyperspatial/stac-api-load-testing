356a7ef9ecebbf384d03e11315be8f709acd4a2a
jonhealy1 <jonathan.d.healy@gmail.com>
2024-03-09 22:48:25 +0800

    v0.1.1, fix sample data location

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,17 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+
 ## [Unreleased]
+
+### Added
+
+### Changed
+
+### Fixed
+
+
+## [v0.1.1]
 
 ### Added
 
@@ -14,6 +24,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ### Changed
 
 ### Fixed
+
+- Fixed sample data location in package files [#4](https://github.com/Healy-Hyperspatial/stac-api-load-balancing/pull/4)
 
 
 ## [v0.1.0]
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """stac-api-load-balancing setup.py."""
 from setuptools import setup
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -20,7 +20,7 @@ setup(
         "bzt",
     ],
     packages=["stac_api_load_balancing"],
-    package_data={"stac_api_load_balancing": ["config_files/*", "data_loader/*"]},
+    # package_data={"stac_api_load_balancing": ["config_files/*", "data_loader/*", "data_loader/setup_data/*"]},
     entry_points={
         "console_scripts": ["stac-api-load-balancing=stac_api_load_balancing.cli:main"]
     },
